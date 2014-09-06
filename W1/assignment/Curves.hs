36feed85c0c899e02e6f73c0461cd2d35c133547
Martin Jørgensen <martinnjorgensen@gmail.com>
2014-09-06 17:36:57 +0200

    Modulified Curves. Made reflect.

--- W1/assignment/Curves.hs
+++ W1/assignment/Curves.hs
@@ -1,16 +1,8 @@
---tests :: [Bool]
---tests = []
--- What where these?
+module Curves where
 
 newtype Point = Point (Double, Double)
   deriving (Show)
 
--- test points
-p1 = Point(0,0)
-p2 = Point(1,2)
-p3 = Point(2,4)
-p4 = Point(4,8)
-p5 = Point(6,16)
 
 point :: (Double, Double) -> Point
 point (x, y) = Point(x, y)
@@ -29,16 +21,8 @@ instance Num (Point) where
   signum (Point (x,y)) = Point (signum x, signum y)
   fromInteger i = Point (fromInteger i, fromInteger i)
 
--- assertion tests
--- (point(0,0) == point(0.009,0.009))
--- (point(0,0) /= point(0.01,0.01))
-
 newtype Curve = Curve [Point]
   deriving (Show)
-
--- test curves
-c1 = curve p1 [p2,p3]
-c2 = curve p4 [p2,p5]
 
 curve :: Point -> [Point] -> Curve
 curve p ps = Curve (p : ps)
@@ -59,8 +43,12 @@ translate (Curve(cs)) p = Curve (map (+ p) cs)
 
 data Axis = Vertical | Horizontal
 
---reflect :: Curve -> Axis -> Double -> Curve
--- TODO: The function. Dashit is going on? :P
+reflect :: Curve -> Axis -> Double -> Curve
+reflect (Curve(ps)) a d = Curve(map (flip' a d) ps)
+  where
+    flip' :: Axis -> Double -> Point -> Point
+    flip' Horizontal d (Point(x,y)) = Point(x     , y*(-1)+(2*d))
+    flip' Vertical   d (Point(x,y)) = Point(x*(-1)+(2*d), y)
 
 --bbox :: Curve -> (Point, Point)
 -- TODO: Actual function, should be easy, linear search through all points,
