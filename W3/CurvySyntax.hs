ccb8442f3a8fc040e7831ded59a8d1b823fdf069
Martin Jørgensen <martinnjorgensen@gmail.com>
2014-09-19 16:01:42 +0200

    Templated.

--- W3/CurvySyntax.hs
+++ W3/CurvySyntax.hs
@@ -1,3 +1,23 @@
 -- Assignment for Week 2 of AP
 -- Martin Jørgensen, tzk173
 -- Casper B. Hansen, fvx507
+
+module CurvySyntax where
+
+import CurveAST
+
+-- Errors can be added ad-libitum.
+data Error = Some | Other
+
+
+
+
+-- parseString "c = (0,0) ++ (5, 42.5)" --should be
+-- Right [Def "c" (Connect (Single (Point (Const 0.0) (Const 0.0)))
+--                         (Single (Point (Const 5.0) (Const 42.5)))) []]
+parseString :: String -> Either Error Program
+parseString str = Right []
+
+-- Parses a file with a Curvy program into a proper Curve.
+parseFile :: FilePath -> IO (Either Error Program)
+parseFile filename = fmap parseString $ readFile filename
