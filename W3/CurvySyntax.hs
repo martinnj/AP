abe77be4b249f14ae4be825eabb057b4c54787ef
Casper B. Hansen <fvx507@alumni.ku.dk>
2014-09-20 20:14:26 +0200

    Progress on the parsers. Ident, Const and Point works.

--- W3/CurvySyntax.hs
+++ W3/CurvySyntax.hs
@@ -6,13 +6,59 @@ module CurvySyntax where
 
 import CurveAST
 
-import Text.Parsec
+import Control.Applicative hiding ((<|>),Const)
+
+-- <|> : either
+
+-- allowed:
+import Text.Parsec.Prim
+import Text.Parsec.Char
+import Text.Parsec.Error
+import Text.Parsec.String
+import Text.Parsec.Combinator
+
+-- disallowed:
+-- import Text.Parsec.Token
+-- import Text.Parsec.Language
+-- import Text.Parsec.Expr
+
+-- utilities
+(<++>) a b = (++) <$> a <*> b   -- concatenation
+(<:>) a b = (:) <$> a <*> b     -- cons
 
 -- Errors can be added ad-libitum.
 data Error = Some | Other
 
+-- parses identifiers
+ident :: GenParser Char st String
+ident = many1 letter <++> opt
+    where opt = option "" $ (letter <|> digit <|> sym) <:> opt
+          sym = oneOf "_"
+
+-- parses floating-point numbers
+number :: GenParser Char st Double
+number = fmap rd (num <++> dec)
+    where rd  = read :: String -> Double
+          num = many1 digit
+          dec = option "" $ char '.' <:> num
+
+expr :: GenParser Char st Expr
+expr =  (do n <- number
+            return (Const n) ) <|>
+        (do e1 <- expr
+            char '+'
+            e2 <- expr
+            return (Add e1 e2 ))
+
+-- matches terminals of a point, discarding symbols
+point :: GenParser Char st Point
+point = pointify <$> (char '(' *> expr) <*>
+                     (char ',' *> expr <* char ')') 
+    where pointify a b = Point a b
 
 
+parser :: String -> Either ParseError Point
+parser str = parse point "Error!" str 
 
 -- parseString "c = (0,0) ++ (5, 42.5)" --should be
 -- Right [Def "c" (Connect (Single (Point (Const 0.0) (Const 0.0)))
