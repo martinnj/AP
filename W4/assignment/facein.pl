18ac4797505787161dd5e3b49297c6815bca9dfd
Martin Jørgensen <martinnjorgensen@gmail.com>
2014-09-23 15:39:22 +0200

    Added start for assignment 4.

---
+++ W4/assignment/facein.pl
@@ -0,0 +1,15 @@
+/** Assignment 4 for Advanced Programming 2014
+ *  Written by Martin Jrgensen, tzk173
+ *  and Casper B. Hansen, xxx111
+ */
+
+/** Demo graph */
+G0 = [person(susan, [reed, jen, andrzej, jessica]),
+      person(reed, [tony, jessica]),
+      person(jessica, [jen]),
+      person(tony, []),
+      person(ken, [andrzej]),
+      person(jen, [tony, susan, jessica]),
+      person(andrzej, [susan, ken])].
+
+/** goodfriends(G ,X ,Y):- */
