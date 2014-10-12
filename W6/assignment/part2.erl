15a1b6233b19e9d0140cb9fb671e47a712aea987
Casper B. Hansen <fvx507@alumni.ku.dk>
2014-10-12 14:53:01 +0200

    corrected numberplate.

--- W6/assignment/part2.erl
+++ W6/assignment/part2.erl
@@ -1,6 +1,6 @@
 % Functions and tests for Assignment 6, Part 2: MusixXMatch Dataset in AP2014
 % Written by Martin Jrgensen, tzk173
-%        and Casper B Hansen, xxxyyy
+%        and Casper B Hansen, fvx507
 
 -module(part2).
 -export([lsum/2, resprint/2]).
